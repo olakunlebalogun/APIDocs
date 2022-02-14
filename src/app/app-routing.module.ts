c6b8050a7acbadce9f70b2b71ad23955554becb1
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2022-02-14 11:23:45 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionFiveComponent } from './components/Pages/section-five/section-five.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'five',component:SectionFiveComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
