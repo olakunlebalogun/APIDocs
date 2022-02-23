b5ac60f1f2565c0f36e974148059306d96895467
Olakunle Balogun <olakunlebalogun247@gmail.com>
2022-02-23 13:13:47 +0100

    made some changes to the some sections

--- src/app/components/entrance/password-reset-complete/password-reset-complete.component.ts
+++ src/app/components/entrance/password-reset-complete/password-reset-complete.component.ts
@@ -3,13 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-password-reset-complete',
   templateUrl: './password-reset-complete.component.html',
-  styleUrls: ['./password-reset-complete.component.scss']
+  styleUrls: ['./password-reset-complete.component.scss'],
 })
 export class PasswordResetCompleteComponent implements OnInit {
+  contOne: string = `{
+  "email": "olakunlebalogun247@gmail.com",
+  "newpassword": "24079",
+  "tokens": "23432"
+}`;
+  headOne: string = `Password Reset Complete`;
 
-  constructor() { }
+  codeOne: string = `curl -X POST "https://swipe.ng:7000/swipepay/entrance/password/reset/complete"
+  -H "accept: */*" -H "Content-Type: application/json" -d "{ \"email\": \"olakunlebalogun247@gmail.com\",
+   \"newpassword\": \"24079\", \"tokens\": \"23432\"}"`;
+  resOne: string = `{
+  "message": "Failed",
+  "code": null,
+  "data": "Oops, record not found. Sorry",
+  "error": null,
+  "error_description": null,
+  "meta": {}
+}`;
 
-  ngOnInit(): void {
-  }
+  codeTwo: string = `curl -X POST "https://swipe.ng:7000/swipepay/entrance/password/reset/complete"
+  -H "accept: */*" -H "Content-Type: application/json" -d "{ \"email\": \"olakunlebalogun247@gmail.com\",
+  \"newpassword\": \"24079\", \"tokens\": \"23432\"}"`;
+  resTwo: string = `{
+  "message": "Failed",
+  "code": null,
+  "data": "Oops, record not found. Sorry",
+  "error": null,
+  "error_description": null,
+  "meta": {}
+}`;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
