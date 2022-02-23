2a2b51a3fc84ea4ceb3d805f519657c545e5324c
larry659 <larryfunmbi26@gmail.com>
2022-02-23 13:01:21 +0100

    preview,pass-change and update authentication pin

--- src/app/components/entrance/preview-photo/preview-photo.component.ts
+++ src/app/components/entrance/preview-photo/preview-photo.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-photo.component.scss']
 })
 export class PreviewPhotoComponent implements OnInit {
-
+  cont:string=`{
+    "photo": "string"
+     }`;
+  head:string = `Preview Photo`;
+  curlSuccess:string = `curl -X GET "https://swipe.ng:7000/swipepay/entrance/preview/photo?photo=gettyimage" -H "accept: */*"`;
+  responseSuccess :string= `{
+    "message": "Success",
+    "code": 200,
+    "data": "photo successfully prviewed",
+    "meta": {}
+  }`;
+  curlError:string=`curl -X GET "https://swipe.ng:7000/swipepay/entrance/preview/photo?photo=gettyimage" -H "accept: */*"`;
+  responseError:string=`{
+    "timestamp": 1645612986551,
+    "status": 500,
+    "error": "Internal Server Error",
+    "path": "/swipepay/entrance/preview/photo"
+  }`;
   constructor() { }
 
   ngOnInit(): void {
